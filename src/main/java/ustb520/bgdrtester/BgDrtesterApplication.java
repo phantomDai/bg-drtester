bc206806e23fc29fcea82543a23f67891ce5b8bc
phantomdai <daihepeng@sina.cn>
2019-07-03 09:26:18 +0800

    Initial commit

---
+++ src/main/java/ustb520/bgdrtester/BgDrtesterApplication.java
@@ -0,0 +1,13 @@
+package ustb520.bgdrtester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgDrtesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgDrtesterApplication.class, args);
+	}
+
+}
